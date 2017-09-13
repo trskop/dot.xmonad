af07e43644b0e68c0d988e59d7a4736e928a9ecc
Peter Trško <peter.trsko@gmail.com>
2017-09-13 14:04:47 +0200

    Configuring workspaces; key bindings; spawning applications

--- app/Main.hs
+++ app/Main.hs
@@ -1,21 +1,109 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 module Main (main)
   where
 
---import Data.Function (($))
+import Prelude ((+))
+
+import Control.Applicative ((*>))
+import Data.Bits ((.|.))
+import Data.Function (($))
+import Data.Monoid ((<>), mconcat)
+import Data.Ord ((<=))
+import Data.String (String)
 import System.IO (IO)
 
+import Data.Map (Map)
+import qualified Data.Map as Map (fromList)
 import XMonad
-    ( XConfig(modMask)
+    ( KeyMask
+    , KeySym
+    , X
+    , XConfig
+        ( XConfig
+        , keys
+        , layoutHook
+        , logHook
+        , manageHook
+        , modMask
+        , terminal
+        , workspaces
+        )
+    , mod1Mask
+    , mod3Mask
     , mod4Mask
+    , shiftMask
+    , spawn
+    , xK_0
+    , xK_l
+    , xK_r
     , xmonad
     )
 import XMonad.Config.Xfce (xfceConfig)
+import XMonad.Hooks.FadeInactive (fadeInactiveLogHook)
+import XMonad.Layout.NoBorders (noBorders)
+import XMonad.ManageHook ((=?), (-->), appName, title)
+import XMonad.Hooks.ManageHelpers (doCenterFloat)
+import qualified XMonad.Operations as Operations (windows)
+import qualified XMonad.StackSet as StackSet (greedyView, shift)
 
-import Local.XMonad ()
+import qualified Local.XMonad as Util (workspaceKeys)
 
 
 main :: IO ()
-main = xmonad xfceConfig
-    { modMask = mod4Mask
-    }
+main = xmonad $ go xfceConfig
+  where
+    go cfg@XConfig{keys, manageHook} = cfg
+        { layoutHook = noBorders (layoutHook cfg)
+        , logHook = fadeInactiveLogHook 0.5 *> logHook cfg
+        , manageHook = manageHook <> mconcat
+            [ title =? "Application Finder" --> doCenterFloat
+            , appName =? "keepassx" --> doCenterFloat
+            ]
+        , workspaces = myWorkspaces
+        , modMask = mod4Mask
+        , keys = myKeys <> keys -- Local definitions have priority.
+        , terminal = "urxvt"
+        }
+
+myWorkspaces :: [String]
+myWorkspaces =
+    [ "α" -- 0
+    , "β" -- 1
+    , "γ" -- 2
+    , "δ" -- 3
+    , "ε" -- 4
+    , "ζ" -- 5
+    , "η" -- 6
+    , "θ" -- 7
+    , "ι" -- 8
+    , "κ" -- 9
+    ]
+
+--myKeys :: XConfig Layout -> Map (KeyMask, KeySym) (X ())
+myKeys :: XConfig a -> Map (KeyMask, KeySym) (X ())
+myKeys cfg@XConfig{modMask} = runCommands <> workspaceKeys
+  where
+    -- mod-r        -- Run command.
+    -- shift-mod-l  -- Lock screen.
+    runCommands = Map.fromList
+        [ ((modMask, xK_r), spawn "xfce4-appfinder --collapsed")
+        , ((modMask .|. shiftMask, xK_l), spawn "xflock4")
+            -- Command xflock4 is a shell script that invokes correct locking
+            -- application, if available.
+        ]
+
+    -- mod-[0..9] -- Switch to workspace N.
+    -- alt-[1..9] -- Move client to workspace N.
+    workspaceKeys = Util.workspaceKeys cfg $ \_cfg name idx ->
+        if idx <= 9
+            then
+                [ ((m, idx + xK_0), Operations.windows $ f name)
+                | (f, m) <-
+                    [ (StackSet.greedyView, modMask)
+                    , (StackSet.shift,      mod1Mask)   -- Left ALT
+                    , (StackSet.shift,      mod3Mask)   -- Right ALT
+                    ]
+                ]
+            else []
--- src/Local/XMonad.hs
+++ src/Local/XMonad.hs
@@ -1,2 +1,26 @@
+{-# LANGUAGE NamedFieldPuns #-}
 module Local.XMonad
   where
+
+import Data.Function (($))
+import qualified Data.List as List (concatMap, zip)
+import Data.String (String)
+import Data.Tuple (uncurry)
+
+import Data.Map (Map)
+import qualified Data.Map as Map (fromList)
+import XMonad
+    ( KeyMask
+    , KeySym
+    , X
+    , XConfig(XConfig, workspaces)
+    )
+
+
+workspaceKeys
+    :: (Num idx, Enum idx)
+    => XConfig a
+    -> (XConfig a -> String -> idx -> [((KeyMask, KeySym), X ())])
+    -> Map (KeyMask, KeySym) (X ())
+workspaceKeys cfg@XConfig{workspaces} f =
+    Map.fromList . List.concatMap (uncurry $ f cfg) $ List.zip workspaces [0..]
